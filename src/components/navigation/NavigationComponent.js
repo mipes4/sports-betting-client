d340e31088eb56102d1caac6c38b812f547a5d9a
Willem Verbuyst <willemverbuyst@gmail.com>
2020-07-28 16:45:14 +0200

    Remove LoggedOut component

--- src/components/navigation/NavigationComponent.js
+++ src/components/navigation/NavigationComponent.js
@@ -1,22 +1,20 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
 import { Navbar } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import { selectToken } from "../../store/user/selectors";
 import LoggedIn from "./LoggedIn";
-import LoggedOut from "./LoggedOut";
 
 export default function NavigationComponent() {
   const token = useSelector(selectToken);
 
-  const loginLogoutControls = !token ? null : <LoggedOut />;
+  const renderControlsLoggedIn = token ? <LoggedIn /> : null;
 
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Navbar.Brand>Sports Betting</Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
-        {loginLogoutControls}
+        {renderControlsLoggedIn}
       </Navbar.Collapse>
     </Navbar>
   );
