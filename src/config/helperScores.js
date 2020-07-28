a84f681c3de317f6aef17cb47bc1bd1b1697a7fa
Willem Verbuyst <willemverbuyst@gmail.com>
2020-07-28 16:14:45 +0200

    Undo the commenting out of helper funtion

--- src/config/helperScores.js
+++ src/config/helperScores.js
@@ -1,37 +1,37 @@
-// export const calculateScore = (results, prediction, scores) => {
-//   const resultMatch = whoWins(results.homeTeam, results.awayTeam);
-//   const resultPred = whoWins(prediction.homeTeam, prediction.awayTeam);
-//   let currentScore = 0;
-//   const guessedHome = results.homeTeam === prediction.homeTeam;
-//   const guessedAway = results.awayTeam === prediction.awayTeam;
+export const calculateScore = (results, prediction, scores) => {
+  const resultMatch = whoWins(results.homeTeam, results.awayTeam);
+  const resultPred = whoWins(prediction.homeTeam, prediction.awayTeam);
+  let currentScore = 0;
+  const guessedHome = results.homeTeam === prediction.homeTeam;
+  const guessedAway = results.awayTeam === prediction.awayTeam;
 
-//   //   console.log("What is scores?", scores);
+  //   console.log("What is scores?", scores);
 
-//   if (resultMatch === resultPred) {
-//     currentScore = currentScore + scores.totoScore;
-//   }
+  if (resultMatch === resultPred) {
+    currentScore = currentScore + scores.totoScore;
+  }
 
-//   if (guessedHome) {
-//     currentScore = currentScore + scores.goalBonus;
-//   }
+  if (guessedHome) {
+    currentScore = currentScore + scores.goalBonus;
+  }
 
-//   if (guessedAway) {
-//     currentScore = currentScore + scores.goalBonus;
-//   }
+  if (guessedAway) {
+    currentScore = currentScore + scores.goalBonus;
+  }
 
-//   if (guessedHome && guessedAway) {
-//     currentScore = currentScore + scores.fullScore;
-//   }
+  if (guessedHome && guessedAway) {
+    currentScore = currentScore + scores.fullScore;
+  }
 
-//   return currentScore;
-// };
+  return currentScore;
+};
 
-// const whoWins = (homeTeam, awayTeam) => {
-//   if (homeTeam > awayTeam) {
-//     return "homeWins";
-//   } else if (homeTeam < awayTeam) {
-//     return "awayWins";
-//   } else {
-//     return "draw";
-//   }
-// };
+const whoWins = (homeTeam, awayTeam) => {
+  if (homeTeam > awayTeam) {
+    return 'homeWins';
+  } else if (homeTeam < awayTeam) {
+    return 'awayWins';
+  } else {
+    return 'draw';
+  }
+};
