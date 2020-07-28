0f6d9ebebcb877c3c768356e1bc0a741985265b3
Willem Verbuyst <willemverbuyst@gmail.com>
2020-07-28 15:47:36 +0200

    Get rid of yellow warnings

--- src/components/pages/Admin.js
+++ src/components/pages/Admin.js
@@ -1,24 +1,22 @@
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { selectToken } from "../../store/user/selectors";
-import { useHistory, Link } from "react-router-dom";
-import { getUserWithStoredToken } from "../../store/user/actions";
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { selectToken } from '../../store/user/selectors';
+import { useHistory, Link } from 'react-router-dom';
 
 export default function Admin() {
   const token = useSelector(selectToken);
   const history = useHistory();
-  const dispatch = useDispatch();
 
   useEffect(() => {
     if (token === null) {
-      history.push("/login");
+      history.push('/login');
     }
   }, [token, history]);
 
   return (
     <div>
       <h1>Admin page</h1>
-      <Link to="/signup" style={{ textAlign: "center" }}>
+      <Link to="/signup" style={{ textAlign: 'center' }}>
         Click here to sign up a new user
       </Link>
     </div>
--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,3 @@
-export const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:4000";
+export const apiUrl = 'http://localhost:4000';
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
-export const apiUrlFootballDemo = "https://www.api-football.com/demo/v2";
+export const apiUrlFootballDemo = 'https://www.api-football.com/demo/v2';
