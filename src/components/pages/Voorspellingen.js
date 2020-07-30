5c28dc0656441a7f45ad62d3fc31b1eaead9957d
Willem Verbuyst <willemverbuyst@gmail.com>
2020-07-30 11:26:28 +0200

    Welcome user to username

--- src/components/pages/Voorspellingen.js
+++ src/components/pages/Voorspellingen.js
@@ -11,16 +11,18 @@ import { useHistory } from "react-router-dom";
 import { appLoading } from "../../store/appState/actions";
 
 export default function Voorspellingen() {
-  // const dispatch = useDispatch();
-  // const matches = useSelector(selectMatches);
+  const dispatch = useDispatch();
+  const matches = useSelector(selectMatches);
   const token = useSelector(selectToken);
   const history = useHistory();
-  // const user = useSelector(selectUser);
+  const user = useSelector(selectUser);
 
-  // useEffect(() => {
-  //   dispatch(fetchMatchesAndPredictions(user.id));
-  //   dispatch(fetchScores());
-  // }, [dispatch, user.id]);
+  useEffect(() => {
+    dispatch(fetchMatchesAndPredictions(user.id));
+    dispatch(fetchScores());
+  }, [dispatch, user.id]);
+
+  console.log(user.id);
 
   useEffect(() => {
     if (token === null) {
@@ -28,41 +30,41 @@ export default function Voorspellingen() {
     }
   }, [token, history]);
 
-  // if (!matches) return dispatch(appLoading());
+  if (!matches) return dispatch(appLoading());
 
-  // const compareMatches = (matchA, matchB) => {
-  //   return matchB.eventTimeStamp - matchA.eventTimeStamp;
-  // };
+  const compareMatches = (matchA, matchB) => {
+    return matchB.eventTimeStamp - matchA.eventTimeStamp;
+  };
 
-  // const sortedMatches = matches.sort(compareMatches);
+  const sortedMatches = matches.sort(compareMatches);
 
-  // const matchesToMatchCard = sortedMatches.map((match) => {
-  //   return (
-  //     <MatchEntry
-  //       key={match.id}
-  //       fixtureId={match.id}
-  //       homeTeamId={match.homeTeamId}
-  //       homeTeamName={match.homeTeamName}
-  //       homeTeamLogo={match.homeTeamLogo}
-  //       goalsHomeTeam={match.goalsHomeTeam}
-  //       awayTeamId={match.awayTeamId}
-  //       awayTeamName={match.awayTeamName}
-  //       awayTeamLogo={match.awayTeamLogo}
-  //       goalsAwayTeam={match.goalsAwayTeam}
-  //       eventTimestamp={match.eventTimeStamp}
-  //       round={match.round}
-  //       status={match.status}
-  //       predictions={match.predictions}
-  //     />
-  //   );
-  // });
+  const matchesToMatchCard = sortedMatches.map((match) => {
+    return (
+      <MatchEntry
+        key={match.id}
+        fixtureId={match.id}
+        homeTeamId={match.homeTeamId}
+        homeTeamName={match.homeTeamName}
+        homeTeamLogo={match.homeTeamLogo}
+        goalsHomeTeam={match.goalsHomeTeam}
+        awayTeamId={match.awayTeamId}
+        awayTeamName={match.awayTeamName}
+        awayTeamLogo={match.awayTeamLogo}
+        goalsAwayTeam={match.goalsAwayTeam}
+        eventTimestamp={match.eventTimeStamp}
+        round={match.round}
+        status={match.status}
+        predictions={match.predictions}
+      />
+    );
+  });
 
   return (
     <Container md={{ span: 11, offset: 0.5 }}>
       <h1>Voorspellingen</h1>
 
-      {/* <Table
-        style={{ fontSize: 12, textAlign: 'left', verticalAlign: 'middle' }}
+      <Table
+        style={{ fontSize: 12, textAlign: "left", verticalAlign: "middle" }}
         striped
         bordered
         hover
@@ -71,7 +73,7 @@ export default function Voorspellingen() {
         responsive="xl"
       >
         <tbody>{matchesToMatchCard}</tbody>
-      </Table> */}
+      </Table>
     </Container>
   );
 }
