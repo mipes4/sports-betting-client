be5ad0ead69c9ec85471250632dde77cbcf6ef0e
Willem Verbuyst <willemverbuyst@gmail.com>
2020-10-02 21:33:17 +0200

    Test appDoneLoading in appStateReducer

--- src/store/appState/tests/reducer.test.js
+++ src/store/appState/tests/reducer.test.js
@@ -1,5 +1,5 @@
 import reducer from "../reducer";
-import { APP_LOADING } from "../actions";
+import { APP_LOADING, APP_DONE_LOADING } from "../actions";
 
 describe("appStateReducer", () => {
   const initialState = {
@@ -12,6 +12,14 @@ describe("appStateReducer", () => {
       const newState = reducer(initialState, action);
       expect(newState).toEqual({ loading: true, message: null });
       expect(newState.loading).toBe(true);
+    });
+  });
+  describe("when given APP_DONE_LOADING action type", () => {
+    test("returns a new state with loading set to false", () => {
+      const action = { type: APP_DONE_LOADING };
+      const newState = reducer(initialState, action);
+      expect(newState).toEqual({ loading: false, message: null });
+      expect(newState.loading).toBe(false);
     });
   });
 });
