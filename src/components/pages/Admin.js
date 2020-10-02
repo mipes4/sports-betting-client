a6b0e762e89c07302add7f206c67e5578971af1a
Willem Verbuyst <willemverbuyst@gmail.com>
2020-10-02 17:09:19 +0200

    Remove sidebar for admin

--- src/components/pages/Admin.js
+++ src/components/pages/Admin.js
@@ -1,9 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { selectToken } from "../../store/user/selectors";
 // Components
-import Sidebar from "../sidebar/Sidebar";
 import Signup from "../signup/Signup";
 // Styles
 import "../../scss/Admin.scss";
@@ -11,8 +10,6 @@ import "../../scss/Admin.scss";
 export default function Admin() {
   const token = useSelector(selectToken);
   const history = useHistory();
-  const [showTask, setShowTask] = useState("signup");
-  const tasks = ["signup", "update data", "change toto status", "other"];
 
   useEffect(() => {
     if (token === null) {
@@ -20,27 +17,7 @@ export default function Admin() {
     }
   }, [token, history]);
 
-  const selectTask = (task) => {
-    console.log(task);
-    setShowTask(task);
-  };
+  // For now just one admintask => signup new user
 
-  const renderEditor = () => {
-    return showTask === "signup" ? (
-      <Signup />
-    ) : showTask === "update data" ? (
-      <h1>Click to update data</h1>
-    ) : showTask === "change toto status" ? (
-      <h1>Change Toto Status</h1>
-    ) : (
-      <h1>Other</h1>
-    );
-  };
-
-  return (
-    <div className="admin__container">
-      <Sidebar onSelect={selectTask} tasks={tasks} />
-      <div className="admin__container--editor">{renderEditor()}</div>
-    </div>
-  );
+  return <Signup />;
 }
