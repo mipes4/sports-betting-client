5b3d12c6327e4fa4eef5db78ad045290e89581c9
Willem Verbuyst <willemverbuyst@gmail.com>
2020-10-02 16:41:04 +0200

    Refactor login page

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
+
 import { login } from "../../store/user/actions";
 import { selectToken } from "../../store/user/selectors";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
+// styles
+import { Form, Button } from "react-bootstrap";
 
 export default function Login() {
   const [email, setEmail] = useState("");
@@ -21,37 +22,23 @@ export default function Login() {
 
   function submitForm(event) {
     event.preventDefault();
-
     dispatch(login(email, password));
     setEmail("");
     setPassword("");
   }
 
   return (
-    <container
-      style={{
-        minHeight: "calc(100vh - 80px)",
-      }}
-    >
-      <div class="col-4" style={{ color: "#fff", marginTop: "30px" }}>
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum."
-      </div>
-
+    <container>
       <div class="col-4 offset-4">
-        <form>
+        <Form>
           <Form.Group controlId="formBasicEmail">
-            <Form.Label>Emailadres</Form.Label>
+            <Form.Label>Email address</Form.Label>
             <Form.Control
               value={email}
               onChange={(event) => setEmail(event.target.value)}
               type="email"
-              // placeholder="Vul emailadres in"
+              type="email"
+              placeholder="Enter email"
               required
             />
           </Form.Group>
@@ -62,16 +49,15 @@ export default function Login() {
               value={password}
               onChange={(event) => setPassword(event.target.value)}
               type="password"
-              // placeholder="Wachtwoord"
+              placeholder="Enter password"
               required
             />
           </Form.Group>
-          <Form.Group className="mt-5">
-            <Button variant="primary" type="submit" onClick={submitForm}>
-              Inloggen
-            </Button>
-          </Form.Group>
-        </form>
+
+          <Button variant="primary" type="submit" onClick={submitForm}>
+            Inloggen
+          </Button>
+        </Form>
       </div>
     </container>
   );
