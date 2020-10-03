3557503a6214431af6ba95dbfd7c437d368ecf07
Willem Verbuyst <willemverbuyst@gmail.com>
2020-10-03 05:17:24 +0200

    Test random actions in appStateReducer

--- src/store/appState/tests/reducer.test.js
+++ src/store/appState/tests/reducer.test.js
@@ -6,6 +6,18 @@ describe("appStateReducer", () => {
     loading: false,
     message: null,
   };
+  describe("if given no state and a random action", () => {
+    test("returns the inital state", () => {
+      const newState = reducer(undefined, { type: APP_DONE_LOADING });
+      expect(newState).toEqual(initialState);
+      expect(newState.message).toBeNull;
+    });
+    test("returns the inital state", () => {
+      const newState = reducer(undefined, { type: APP_LOADING });
+      expect(newState.loading).toBe(true);
+      expect(newState.message).toBeNull;
+    });
+  });
   describe("when given APP_LOADING action type", () => {
     test("returns a new state with loading set to true", () => {
       const action = { type: APP_LOADING };
