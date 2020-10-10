f9c578bddc3fed06afdf0f46b6de546f27a69a48
Willem Verbuyst <willemverbuyst@gmail.com>
2020-10-10 17:09:22 +0200

    Test log out userReducer'

--- src/store/user/tests/reducer.test.js
+++ src/store/user/tests/reducer.test.js
@@ -23,4 +23,21 @@ describe("#userReducer", () => {
       expect(newState).toEqual(userWithToken);
     });
   });
+  describe("#LOG_OUT action type", () => {
+    const initialState = {
+      token: null,
+      email: null,
+    };
+    const userWithToken = {
+      token: "test_token",
+      email: "test_email",
+      userDetails: "test_user_details",
+    };
+    test("should return initialState", () => {
+      const action = { type: LOG_OUT };
+      const newState = reducer(userWithToken, action);
+      expect(newState).toEqual(initialState);
+      expect(newState.token).toBeNull();
+    });
+  });
 });
